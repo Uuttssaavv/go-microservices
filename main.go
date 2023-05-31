79464cdaf49e3f09d2142843ffc62f4b57a758fe
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-01 01:46:23 +0545

    initialized databse

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"go-microservices/common/database"
+	"go-microservices/common/utilities"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	router := SetupAppRouter()
+	log.Fatal(router.Run(":" + utilities.GodotEnv("GO_PORT")))
+
+}
+
+func SetupAppRouter() *gin.Engine {
+
+	dbService := database.NewDBService()
+	dbService.Connection()
+
+	router := gin.Default()
+
+	gin.SetMode(gin.DebugMode)
+
+	apiGroup := router.Group("api/v1")
+	
+	logrus.Info("ApiGroup=>", apiGroup)
+
+	return router
+}
