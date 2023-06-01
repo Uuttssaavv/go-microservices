014bb9dbced37c661d6813a13b73f0559edbc5e8
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-01 15:38:27 +0545

    segregated handlers in files

---
+++ auth-service/handlers/login.auth.handler.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"go-microservices/auth-service/models"
+	"go-microservices/common/utilities"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (handler *handler) LoginHandler(context *gin.Context) {
+	//parse the body
+	var input models.UserInputModel
+
+	err := context.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := utilities.ParseBindingError(err)
+		utilities.ValidatorErrorResponse(context, http.StatusBadRequest, errors)
+		return
+	}
+	// call the service
+	loginEntity, statusCode := handler.service.Login(&input)
+
+	switch statusCode {
+	case http.StatusOK:
+		token, err := utilities.Sign(map[string]interface{}{"id": loginEntity.ID, "email": loginEntity.Email}, 24*60*1)
+
+		if err != nil {
+			utilities.APIResponse(context, "Cannt generate token", http.StatusInternalServerError, err)
+			return
+		}
+		var userResponse models.UserResponse
+
+		utilities.Unmarshal(loginEntity, &userResponse)
+		userResponse.Token = token
+
+		utilities.APIResponse(context, "Login successful", http.StatusOK, userResponse)
+
+		return
+	case http.StatusNotFound:
+		utilities.APIResponse(context, "User does not exist", http.StatusNotFound, err)
+
+		return
+	case http.StatusUnauthorized:
+		utilities.APIResponse(context, "Invalid email or password", http.StatusNotFound, err)
+	}
+
+}
---
+++ auth-service/handlers/register.auth.handler.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"go-microservices/auth-service/models"
+	"go-microservices/common/utilities"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func (handler *handler) RegisterHandler(context *gin.Context) {
+	//parse the body
+	var input models.UserInputModel
+
+	err := context.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := utilities.ParseBindingError(err)
+		utilities.ValidatorErrorResponse(context, http.StatusBadRequest, errors)
+		return
+	}
+	// call the service
+	registerEntity, statusCode := handler.service.Register(&input)
+	
+	switch statusCode {
+	case http.StatusCreated:
+
+		token, err := utilities.Sign(map[string]interface{}{"id": registerEntity.ID, "email": registerEntity.Email}, 24*60*1)
+
+		if err != nil {
+			utilities.APIResponse(context, "Cannt generate token", http.StatusInternalServerError, err)
+			return
+		}
+
+		var userResponse models.UserResponse
+
+		utilities.Unmarshal(registerEntity, &userResponse)
+		userResponse.Token = token
+
+		utilities.APIResponse(context, "Registered the user", http.StatusCreated, userResponse)
+		return
+
+	case http.StatusConflict:
+		utilities.APIResponse(context, "User already exists", http.StatusConflict, err)
+		return
+
+	case http.StatusExpectationFailed:
+		utilities.APIResponse(context, "Unable to create user", http.StatusExpectationFailed, err)
+		return
+	}
+
+}
