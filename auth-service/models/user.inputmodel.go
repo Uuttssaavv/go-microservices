34d1f90c3c7ff20bf2f82ae2f1d56e2b4e0d5db5
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-01 15:26:31 +0545

    added binding condition

--- auth-service/models/user.inputmodel.go
+++ auth-service/models/user.inputmodel.go
@@ -2,13 +2,7 @@ package models
 
 type UserInputModel struct {
 	ID       uint   `json:"id"`
-	Email    string `json:"email" binding:"required,email"`
-	Phone    string `json:"phone" binding:"len=10"`
+	Email    string `json:"email" binding:"omitempty,email"`
+	Phone    string `json:"phone" binding:"omitempty,numeric,len=10"`
 	Password string `json:"password" binding:"required,min=6,max=16"`
-}
-
-func (input *UserInputModel) Validate() (*UserInputModel, error) {
-	
-	//  perform validation
-	return nil, nil
 }
