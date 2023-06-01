94d9abc0dc8fe9ec800e9f45df4334344fe2aaa7
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-01 13:13:01 +0545

    created custom response format

---
+++ common/utilities/response.go
@@ -0,0 +1,35 @@
+package utilities
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Responses struct {
+	StatusCode int         `json:"statusCode"`
+	Method     string      `json:"method"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+}
+
+type ErrorResponse struct {
+	StatusCode int         `json:"statusCode"`
+	Method     string      `json:"method"`
+	Errors     interface{} `json:"errors"`
+}
+
+func APIResponse(ctx *gin.Context, Message string, StatusCode int, Method string, Data interface{}) {
+
+	jsonResponse := Responses{
+		StatusCode: StatusCode,
+		Method:     Method,
+		Message:    Message,
+		Data:       Data,
+	}
+
+	if StatusCode >= 400 {
+		ctx.JSON(StatusCode, jsonResponse)
+		defer ctx.AbortWithStatus(StatusCode)
+	} else {
+		ctx.JSON(StatusCode, jsonResponse)
+	}
+}
