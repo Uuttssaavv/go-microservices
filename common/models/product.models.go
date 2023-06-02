e6f70732ba97fa4e60beff51dadcce3de0973fa5
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-02 22:29:11 +0545

    created product entity

--- common/models/product.models.go
+++ common/models/product.models.go
@@ -1,1 +1,13 @@
 package models
+
+type ProductEntity struct{
+	ID uint   `gorm:"primary key"`
+	Title string  `gorm:"not null"`
+	Description string  `gorm:"not null"`
+	Price float64  `gorm:"not null"`
+	Image string  `gorm:""`
+	Discount float64  `gorm:"not null"`
+	TotalQuanitity uint  `gorm:"not null"`
+	StockQuantity uint  `gorm:"not null"`
+}
+
