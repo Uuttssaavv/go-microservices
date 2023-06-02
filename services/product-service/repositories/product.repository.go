d9ee6327076cfde75ecee8a10dedb49967e59009
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-02 23:27:09 +0545

    created product repository to handle product db operation

--- services/product-service/repositories/product.repository.go
+++ services/product-service/repositories/product.repository.go
@@ -1,1 +1,15 @@
 package repositories
+
+import "github.com/jinzhu/gorm"
+
+type Repository interface {
+	//
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewProductRepository(db *gorm.DB) *repository {
+	return &repository{db: db}
+}
