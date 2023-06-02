15a8eb81ddceeae7e52b51ffb2d14c5d5b3d7429
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-02 23:26:43 +0545

    created product handler to handle product logics

--- services/product-service/handlers/product.handler.go
+++ services/product-service/handlers/product.handler.go
@@ -1,1 +1,11 @@
 package handlers
+
+import "go-microservices/services/product-service/services"
+
+type handler struct {
+	services services.Service
+}
+
+func NewProductHandler(service services.Service) *handler {
+	return &handler{services: service}
+}
