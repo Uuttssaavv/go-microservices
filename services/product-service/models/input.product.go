c1ee10bcdea992ae54d5d1edf3977898e37af0ed
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-04 21:13:44 +0545

    altered the type of image for proper binding

--- services/product-service/models/input.product.go
+++ services/product-service/models/input.product.go
@@ -1,12 +1,14 @@
 package models
 
+import "mime/multipart"
+
 type ProductInput struct {
-	ID             uint    `gorm:"primary key" json:"id"`
-	Title          string  `gorm:"not null" binding:"required" json:"title"`
-	Description    string  `gorm:"not null" binding:"required" json:"description"`
-	Price          float64 `gorm:"not null" binding:"required,numeric" json:"price"`
-	Image          string  `gorm:"" binding:"required" json:"image"`
-	Discount       float64 `gorm:"not null; default:0" binding:"omitempty,numeric" json:"discount"`
-	TotalQuanitity uint    `gorm:"not null" binding:"" json:"total_quantities"`
-	StockQuantity  uint    `gorm:"not null" binding:"" json:"stock_quantities"`
+	ID             uint    `gorm:"autoIncrement; primary key" json:"id" form:"id"`
+	Title          string  `gorm:"not null" binding:"required" json:"title" form:"title"`
+	Description    string  `gorm:"not null" binding:"required" json:"description" form:"description"`
+	Price          float64 `gorm:"not null" binding:"required,numeric" json:"price" form:"price"`
+	Image           *multipart.FileHeader  `gorm:"" binding:"-" json:"-" form:"image"`
+	Discount       float64 `gorm:"not null; default:0" binding:"omitempty,numeric" json:"discount" form:"discount"`
+	TotalQuanitity uint    `gorm:"not null" binding:"required" json:"total_quantities" form:"total_quantities"`
+	StockQuantity  uint    `gorm:"not null default:0" binding:"" json:"stock_quantities" form:"stock_quantities"`
 }
