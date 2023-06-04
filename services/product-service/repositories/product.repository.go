df035281aac2a774f383eb7bc642f5aafe5af824
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-04 21:12:46 +0545

    added signatures in Repository interface

--- services/product-service/repositories/product.repository.go
+++ services/product-service/repositories/product.repository.go
@@ -1,14 +1,24 @@
 package repositories
 
-import "github.com/jinzhu/gorm"
+import (
+	entities "go-microservices/common/models"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Repository interface {
-	//
+	CreateProduct(entities.ProductEntity) (*entities.ProductEntity, int)
+	UpdateProduct(entities.ProductEntity) (*entities.ProductEntity, int)
+	GetProductById(uint) (*entities.ProductEntity, int)
+	GetProductsByUser(uint) (*[]entities.ProductEntity, int)
+	GetAllProducts() (*[]entities.ProductEntity, int)
+	DeleteProduct(uint) int
 }
 
 type repository struct {
 	db *gorm.DB
 }
+
 
 func NewProductRepository(db *gorm.DB) *repository {
 	return &repository{db: db}
