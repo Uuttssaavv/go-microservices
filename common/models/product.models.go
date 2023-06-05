d78ec0b347495c1db8ba707502586f6e42b0bcc2
Uuttssaavv <ghimireutsav11@gmail.com>
2023-06-05 09:55:37 +0545

    added association with userEntitity

--- common/models/product.models.go
+++ common/models/product.models.go
@@ -1,17 +1,29 @@
 package models
 
-import "time"
+import (
+	"time"
 
-type ProductEntity struct{
-	ID uint   `gorm:"primary key"`
-	Title string  `gorm:"not null"`
-	Description string  `gorm:"not null"`
-	Price float64  `gorm:"not null"`
-	Image string  `gorm:""`
-	Discount float64  `gorm:"not null"`
-	TotalQuanitity uint  `gorm:"not null"`
-	StockQuantity uint  `gorm:"not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	"github.com/jinzhu/gorm"
+)
+
+type ProductEntity struct {
+	ID               uint       `gorm:"primary key" json:"id"`
+	Title            string     `gorm:"not null" json:"title"`
+	Description      string     `gorm:"not null" json:"description"`
+	Price            float64    `gorm:"not null" json:"price"`
+	Image            string     `gorm:"" json:"-"`
+	Discount         float64    `gorm:"not null" json:"discount"`
+	TotalQuanitities uint       `gorm:"not null" json:"total_quantities"`
+	StockQuantities  uint       `gorm:"not null" json:"stock_quantities"`
+	UserID           uint       `gorm:"foreignkey:UserID" json:"-"`
+	User             UserEntity `gorm:"foreignkey:UserID" json:"user"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
 }
 
+func (entity *ProductEntity) BeforeCreate(db *gorm.DB) error {
+	entity.CreatedAt = time.Now().Local()
+	entity.UpdatedAt = time.Now().Local()
+
+	return nil
+}
